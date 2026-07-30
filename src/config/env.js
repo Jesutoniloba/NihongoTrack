450bc73eb10b2d1e9f0036ca82a1dd318591bd4f
Jesutoniloba Oladipo <jesutonilobaoladipo@gmail.com>
2026-07-30 10:04:28 +0100

    implemented access token

--- src/config/env.js
+++ src/config/env.js
@@ -4,4 +4,8 @@ dotenv.config();
 export const env = {
   PORT: process.env.PORT,
   DATABASE_URL: process.env.DATABASE_URL,
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN,
+  REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN,
 };
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 import argon2 from "argon2";
 import pool from "../config/db.js";
+import { env } from "../config/env.js";
 import { createUserService } from "../models/user-model.js";
 
 export async function register(req, res) {
@@ -12,11 +13,12 @@ export async function register(req, res) {
     .json({ message: "User created Sucessfully", newUser: newUser.username });
 }
 
+//async function verifyUser(req, res) {}
 export async function login(req, res) {
   const result = await pool.query(`SELECT email FROM users WHERE email = $1`, [
     req.body.email,
   ]);
-  const user = result.rows[0].email;
+  const user = result.rows[0];
   if (!user) return res.status(404).send(" User not found");
   try {
     const hash = await pool.query(
@@ -25,15 +27,21 @@ export async function login(req, res) {
     );
     const storedHash = hash.rows[0].password_hash;
     if (await argon2.verify(storedHash, req.body.password)) {
-      res
-        .status(200)
-        .json({ user: user, message: "AUTHENTICATION SUCESSFULL" });
+      //   res
+      //   .status(200)
+      //   .json({ user: user, message: "AUTHENTICATION SUCESSFULL" });
     } else {
-      res.status(403).send("Authentication Failed");
+      // res.status(403).send("Authentication Failed");
     }
   } catch (err) {
     res.send("Authentication Error");
   }
+  const accessToken = generateAccessToken(user);
+  res.json({ accessToken: accessToken });
+}
+
+function generateAccessToken(user) {
+  return jwt.sign(user, env.ACCESS_TOKEN_SECRET, { expiresIn: "20s" });
 }
 
 export async function logout(req, res) {}
